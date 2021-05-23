d86086b9da180e57cf1db0d838829ab927e8ca6b
최덕경 <hjcdg197@gmail.com>
2021-05-23 19:08:11 +0900

    [Change] 본 컴포넌트에 코드 옮기고 테스트 컴포넌트 삭제

--- pages/bubble-effect.tsx
+++ pages/bubble-effect.tsx
@@ -2,15 +2,15 @@ import { useEffect } from 'react';
 
 function BubbleEffect() {
   useEffect(() => {
-    const c1 = document.getElementById('c1') as HTMLCanvasElement;
-    const c2 = document.getElementById('c2') as HTMLCanvasElement;
-    const context1 = c1.getContext('2d') as CanvasRenderingContext2D;
-    const context2 = c2.getContext('2d') as CanvasRenderingContext2D;
+    const canvasBackground = document.getElementById('canvas-background') as HTMLCanvasElement;
+    const canvasFloating = document.getElementById('canvas-floating') as HTMLCanvasElement;
+    const contextBackground = canvasBackground.getContext('2d') as CanvasRenderingContext2D;
+    const contextFloating = canvasFloating.getContext('2d') as CanvasRenderingContext2D;
 
-    let canvasWidth = c1.width = c2.width = window.innerWidth;
-    let canvasHeight = c1.height = c2.height = window.innerHeight;
-    let sizeBase = canvasWidth + canvasHeight;
-    let parts = [] as {
+    let canvasWidth: number;
+    let canvasHeight: number;
+    let sizeBase: number;
+    let floatingBubbles = [] as {
       x: number;
       y: number;
       radius: number;
@@ -27,7 +27,21 @@ function BubbleEffect() {
       return `hsla(${h}, ${s}%, ${l}%, ${a})`;
     }
 
-    function drawCanvas() {
+    function initCanvas() {
+      canvasWidth = canvasBackground.width = canvasFloating.width = window.innerWidth;
+      canvasHeight = canvasBackground.height = canvasFloating.height = window.innerHeight;
+      sizeBase = canvasWidth + canvasHeight;
+      floatingBubbles = [];
+    }
+
+    function drawCanvasBackground() {
+      /* 캔버스 지우기 */
+      contextBackground.clearRect(0, 0, canvasWidth, canvasHeight);
+
+      /* 채우기 색과 그림자 색이 섞이도록 함 */
+      contextBackground.globalCompositeOperation = 'lighter';
+
+      /* 그림자 색 (색상, 채도, 명도, 불투명도) */
       const hueMin = rand(0, 360);
       const hueMax = hueMin + 100;
       const saturationMin = 10;
@@ -36,98 +50,138 @@ function BubbleEffect() {
       const lightnessMax = 50;
       const alphaMin = 0.1;
       const alphaMax = 0.5;
+
+      /* 그림자 번짐 정도 */
       const blurMin = 10;
       const blurMax = sizeBase * 0.04;
+
+      /* 원 중심 좌표 */
+      const xMin = 0;
+      const xMax = canvasWidth;
+      const yMin = 0;
+      const yMax = canvasHeight;
+
+      /* 원 반지름 */
       const radiusMin = 1;
       const radiusMax = sizeBase * 0.04;
 
-      context1.clearRect(0, 0, canvasWidth, canvasHeight);
-      context1.globalCompositeOperation = 'lighter';
-
-      let count = Math.floor(sizeBase * 0.1);
-      while (count--) {
+      /* 백그라운드 거품 그리기 */
+      for (let i = 0; i < Math.floor(sizeBase * 0.2); i++) {
         const hue = rand(hueMin, hueMax);
         const saturation = rand(saturationMin, saturationMax);
         const lightness = rand(lightnessMin, lightnessMax);
         const alpha = rand(alphaMin, alphaMax);
         const blur = rand(blurMin, blurMax);
-        const x = rand(0, canvasWidth);
-        const y = rand(0, canvasHeight);
+        const x = rand(xMin, xMax);
+        const y = rand(yMin, yMax);
         const radius = rand(radiusMin, radiusMax);
 
-        context1.shadowColor = hsla(hue, saturation, lightness, alpha);
-        context1.shadowBlur = blur;
-        context1.beginPath();
-        context1.arc(x, y, radius, 0, 2 * Math.PI);
-        context1.closePath();
-        context1.fill();
+        contextBackground.shadowColor = hsla(hue, saturation, lightness, alpha);
+        contextBackground.shadowBlur = blur;
+        contextBackground.beginPath();
+        contextBackground.arc(x, y, radius, 0, 2 * Math.PI);
+        contextBackground.closePath();
+        contextBackground.fillStyle = '#000000';
+        contextBackground.fill();
       }
+    }
 
+    function constructFloatingBubbles() {
+      /* 원 중심 좌표 */
+      const xMin = 0;
+      const xMax = canvasWidth;
+      const yMin = 0;
+      const yMax = canvasHeight;
+
+      /* 원 반지름 */
+      const radiusMin = 1;
+      const radiusMax = sizeBase * 0.03;
+
+      /* 원 이동 방향 */
+      const angleMin = 0;
+      const angleMax = 2 * Math.PI;
+
+      /* 원 이동 속력 */
+      const velMin = 0.1;
+      const velMax = 0.5;
+
+      /* 원 불투명도 */
+      const tickMin = 0;
+      const tickMax = 10000;
+
+      /* 움직이는 거품 그리기 위한 정보 구성 */
       for (let i = 0; i < Math.floor(sizeBase * 0.03); i++) {
-        parts.push({
-          x: rand(0, canvasWidth),
-          y: rand(0, canvasHeight),
-          radius: rand(1, sizeBase * 0.03),
-          angle: rand(0, 2 * Math.PI),
-          vel: rand(0.1, 0.5),
-          tick: rand(0, 10000)
+        floatingBubbles.push({
+          x: rand(xMin, xMax),
+          y: rand(yMin, yMax),
+          radius: rand(radiusMin, radiusMax),
+          angle: rand(angleMin, angleMax),
+          vel: rand(velMin, velMax),
+          tick: rand(tickMin, tickMax)
         });
       }
     }
 
-    function initCanvas() {
-      canvasWidth = c1.width = c2.width = window.innerWidth;
-      canvasHeight = c1.height = c2.height = window.innerHeight;
-      sizeBase = canvasWidth + canvasHeight;
-      parts = [];
+    function drawFloatingBubbles() {
+      requestAnimationFrame(drawFloatingBubbles);
 
-      drawCanvas();
-    }
+      /* 캔버스 지우기 */
+      contextFloating.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    function loop() {
-      requestAnimationFrame(loop);
+      /* 백그라운드 거품 그리기 */
+      contextFloating.globalCompositeOperation = 'source-over';
+      contextFloating.shadowBlur = 0;
+      contextFloating.drawImage(canvasBackground, 0, 0);
 
-      context2.clearRect(0, 0, canvasWidth, canvasHeight);
-      context2.globalCompositeOperation = 'source-over';
-      context2.shadowBlur = 0;
-      context2.drawImage(c1, 0, 0);
-      context2.globalCompositeOperation = 'lighter';
-      context2.shadowBlur = 15;
-      context2.shadowColor = '#fff';
+      /* 움직이는 거품 그리기 준비 */
+      contextFloating.globalCompositeOperation = 'lighter';
+      contextFloating.shadowColor = '#ffffff';
+      contextFloating.shadowBlur = 15;
 
-      let i = parts.length;
-      while (i--) {
-        const part = parts[i];
+      /* 움직이는 거품 그리기 */
+      for (let i = 0; i < floatingBubbles.length; i++) {
+        const floatingBubble = floatingBubbles[i];
 
-        part.x += Math.cos(part.angle) * part.vel;
-        part.y += Math.sin(part.angle) * part.vel;
-        part.angle += rand(-0.05, 0.05);
+        /* 거품 그리기 */
+        contextFloating.beginPath();
+        contextFloating.arc(floatingBubble.x, floatingBubble.y, floatingBubble.radius, 0, 2 * Math.PI);
+        contextFloating.closePath();
+        contextFloating.fillStyle = hsla(0, 0, 100, 0.075 + Math.cos(floatingBubble.tick * 0.02) * 0.05);
+        contextFloating.fill();
 
-        context2.beginPath();
-        context2.arc(part.x, part.y, part.radius, 0, 2 * Math.PI);
-        context2.fillStyle = hsla(0, 0, 100, 0.075 + Math.cos(part.tick * 0.02) * 0.05);
-        context2.fill();
+        /* 거품 이동 */
+        floatingBubble.x += floatingBubble.vel * Math.cos(floatingBubble.angle);
+        floatingBubble.y += floatingBubble.vel * Math.sin(floatingBubble.angle);
+        if (floatingBubble.x - floatingBubble.radius > canvasWidth) floatingBubble.x = -floatingBubble.radius;
+        if (floatingBubble.x + floatingBubble.radius < 0) floatingBubble.x = canvasWidth + floatingBubble.radius;
+        if (floatingBubble.y - floatingBubble.radius > canvasHeight) floatingBubble.y = -floatingBubble.radius;
+        if (floatingBubble.y + floatingBubble.radius < 0) floatingBubble.y = canvasHeight + floatingBubble.radius;
 
-        if (part.x - part.radius > canvasWidth) part.x = -part.radius;
-        if (part.x + part.radius < 0) part.x = canvasWidth + part.radius;
-        if (part.y - part.radius > canvasHeight) part.y = -part.radius;
-        if (part.y + part.radius < 0) part.y = canvasHeight + part.radius;
+        /* 거품 이동 방향 변경 */
+        floatingBubble.angle += rand(-0.05, 0.05);
 
-        part.tick++;
+        /* 거품 불투명도 조절 */
+        floatingBubble.tick++;
       }
     }
 
-    /* Initialization */
     initCanvas();
-    loop();
-    window.addEventListener('resize', initCanvas);
+    drawCanvasBackground();
+    constructFloatingBubbles();
+    drawFloatingBubbles();
+
+    window.addEventListener('resize', () => {
+      initCanvas();
+      drawCanvasBackground();
+      constructFloatingBubbles();
+    });
   }, []);
 
   return (
     <>
       <div className="container">
-        <canvas id="c1"></canvas>
-        <canvas id="c2"></canvas>
+        <canvas id="canvas-background"></canvas>
+        <canvas id="canvas-floating"></canvas>
       </div>
       <style jsx>{`
         canvas {
@@ -137,11 +191,11 @@ function BubbleEffect() {
           bottom: 0;
           left: 0;
         }
-        #c1 {
+        #canvas-background {
           opacity: 0;
         }
-        #c2 {
-          background: #000;
+        #canvas-floating {
+          background: #000000;
         }
       `}</style>
     </>
